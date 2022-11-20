74126a8247623436ca227a327423d102bc97272e
CircuitSacul <circuitsacul@gmail.com>
2022-11-19 21:44:40 -0500

    add transition to tabs

--- app.vue
+++ app.vue
@@ -2,7 +2,7 @@
     <Html class="bg-slate-300"></Html>
 
     <div class="container bg-slate-200 m-auto sm:my-6 sm:rounded-xl drop-shadow-sm sm:drop-shadow-xl">
-        <div class="tabs bg-slate-200 p-4 tabs-boxed bg-transparent">
+        <div class="tabs tabs-boxed bg-slate-200 p-4 bg-transparent">
             <NuxtLink to="/" class="tab">About Me</NuxtLink>
             <NuxtLink to="/projects" class="tab">My Projects</NuxtLink>
         </div>
@@ -16,7 +16,12 @@
 </template>
 
 <style lang="postcss" scoped>
+.tab {
+    @apply transition-colors;
+    @apply rounded-lg;
+}
+
 a.router-link-exact-active {
-    @apply tab-active
+    @apply tab-active;
 }
 </style>
